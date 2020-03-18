e6b3e97519743fe76744b809be63f81b21c238cb
Renann Souza <renann_puera@hotmail.com>
2020-03-18 13:22:25 -0300

    Initial Project Structure

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function src() {
+  return (
+    <View>
+      <Text>Teste</Text>
+    </View>
+  );
+}
